846aec4a6d457d7b78f02fd8fe40d4eea81d0bbe
Ritesh <riteshnzee@gmail.com>
2024-07-29 23:12:29 +0530

    successfully connected to weather api

--- src/component/Hourlyweather.js
+++ src/component/Hourlyweather.js
@@ -1,42 +1,47 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
-const Hourlyweather = () => {
+
+const Hourlyweather = (props) => {
+
+  const {latitude, longitude} = props
+  const [hourly, setHourly] = useState([])
+  const celcius = (kelvin) =>{
+    return (kelvin - 273.15).toFixed(0)
+  }
+
+  const fetchHourlyWeather = async () => {
+    if (!longitude || !latitude) return; // Check if longitude and latitude are available
+    let url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=b4497a8b1ebf1e766ee5d51e5b9d801e`
+    let data = await fetch(url)
+    let parsedData = await data.json()
+    let finalData = parsedData.list.slice(0,6)
+    console.log(finalData)
+    setHourly(finalData)
+  }
+
+  useEffect(() => {
+    fetchHourlyWeather()
+    
+  }, [latitude,longitude])
+  
+  
   return (
     <>
-    <div className='grid grid-cols-6 grid-rows-[15%_1fr] h-full pb-3'>
-      <div className='col-span-6 ml-3 mt-2'>Today's Forcast</div>
-      <div className='flex flex-col justify-around content-center p-3 text-center '>
-        <p className=''>12:00</p>
-        <img src="" alt="" />
-        <p className = "text-2xl">32&deg;</p>
-        </div>
-        <div className='flex flex-col justify-around content-center p-3 text-center border-l-2 border-white'>
-        <p className=''>12:00</p>
-        <img src="" alt="" />
-        <p className = "text-2xl">32&deg;</p>
-        </div>
-        <div className='flex flex-col justify-around content-center p-3 text-center border-l-2 border-white'>
-        <p className=''>12:00</p>
-        <img src="" alt="" />
-        <p className = "text-2xl">32&deg;</p>
-        </div>
-        <div className='flex flex-col justify-around content-center p-3 text-center border-l-2 border-white'>
-        <p className=''>12:00</p>
-        <img src="" alt="" />
-        <p className = "text-2xl">32&deg;</p>
-        </div>
-        <div className='flex flex-col justify-around content-center p-3 text-center border-l-2 border-white'>
-        <p className=''>12:00</p>
-        <img src="" alt="" />
-        <p className = "text-2xl">32&deg;</p>
-        </div>
-        <div className='flex flex-col justify-around content-center p-3 text-center border-l-2 border-white'>
-        <p className=''>12:00</p>
-        <img src="" alt="" />
-        <p className = "text-2xl">32&deg;</p>
-        </div>
-        
+    <div className='grid grid-cols-6 grid-rows-[15%_1fr] h-full gap-3 pb-3'>
       
+        <div className='col-span-6 pl-5 pt-1 bg-custom-dark-blue rounded-2xl'>Today's Forcast</div>
+        {hourly.map((element) => {
+          let time = element.dt_txt
+          let hours = new Date(time).getHours()
+          return (<div key = {element.dt} className='flex flex-col justify-around content-center p-3 text-center   bg-custom-dark-blue rounded-2xl'>
+          <p className='' >{hours}:00</p>
+          <img src="" alt="" />
+          <p className = "text-2xl">{celcius(element.main.temp)}&deg;</p>
+          </div>)
+
+        })}
     </div>
     </>
   )
